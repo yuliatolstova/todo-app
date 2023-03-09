c0bb6c11b05b8e716b2637d5187d2136b4317797
yuliatolstova <tolstovaulia257@gmail.com>
2023-03-09 20:16:42 +0700

    sd

---
+++ src/components/App/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+
+import TodoList from '../todo-list.js'
+import AppHeader from '../app-header.js'
+import AppFooter from '../app-footer.js'
+
+export default class App extends Component {
+  maxId = 100
+
+  createTodoItem = (label) => ({ label, completed: false, id: this.maxId++ })
+
+  state = {
+    todoData: [],
+    filter: 'all',
+  }
+
+  onDelete = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      return {
+        todoData: [...todoData.slice(0, idx), ...todoData.slice(idx + 1)],
+      }
+    })
+  }
+
+  onAdd = (label) => {
+    const newItem = this.createTodoItem(label)
+    this.setState(({ todoData }) => ({
+      todoData: [...todoData, newItem],
+    }))
+  }
+
+  onToggleCompleted = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const oldItem = todoData[idx]
+      const newItem = { ...oldItem, completed: !oldItem.completed }
+      return {
+        todoData: [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)],
+      }
+    })
+  }
+
+  onFilterChange = (filter) => {
+    this.setState({ filter })
+  }
+
+  filter(items, filter) {
+    switch (filter) {
+      case 'all':
+        return items
+      case 'active':
+        return items.filter((item) => !item.completed)
+      case 'completed':
+        return items.filter((item) => item.completed)
+      default:
+        return items
+    }
+  }
+
+  render() {
+    const { todoData, filter } = this.state
+    const visibleItems = this.filter(todoData, filter)
+    const todoCount = todoData.filter((el) => !el.completed).length
+    return (
+      <section className="todoapp">
+        <AppHeader onAdd={this.onAdd} />
+
+        <section className="main">
+          <TodoList todoData={visibleItems} onDelete={this.onDelete} onToggleCompleted={this.onToggleCompleted} />
+        </section>
+        <AppFooter
+          todoCount={todoCount}
+          onDelete={this.onDelete}
+          todoData={todoData}
+          filter={filter}
+          onFilterChange={this.onFilterChange}
+        />
+      </section>
+    )
+  }
+}
